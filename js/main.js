c34bbdf02e0de250ed0d167bfae8dc8633c578d3
Dylan Orrell <34012156+dylorr@users.noreply.github.com>
2020-03-30 18:25:23 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,8 @@ function addevent(event) {
     let x = ((event.clientX - rect.left)/document.getElementById('pitch').offsetWidth); //x position within the element.
     let y = ((event.clientY - rect.top)/document.getElementById('pitch').offsetHeight);  //y position within the element.
     
-  
+    drawCoordinates(x,y);
+
       
     // let coords2 = "W = " + x + " Y = " + y;
     
@@ -145,7 +146,5 @@ function addevent(event) {
         }
     }, false);
     
-drawCoordinates(x,y);
-
 }
   //# sourceURL=userscript.js
