0993f94d48b61fc11aa0c0648813abf3587b695b
Dylan Orrell <34012156+dylorr@users.noreply.github.com>
2020-03-30 19:49:28 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function addevent(event) {
     let x = ((event.clientX - rect.left)/document.getElementById('pitch').offsetWidth); //x position within the element.
     let y = ((event.clientY - rect.top)/document.getElementById('pitch').offsetHeight);  //y position within the element.
     
-    drawCoordinates(x*100,y*100);
+    drawCoordinates(x,y);
 
       
     // let coords2 = "W = " + x + " Y = " + y;
