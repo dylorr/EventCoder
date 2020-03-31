93774b084c8b065d7c4ba7b853b42a33f2b01fff
Dylan Orrell <34012156+dylorr@users.noreply.github.com>
2020-03-30 20:06:01 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function addevent(event) {
     let xplot = event.clientX - rect.left;
     let yplot = event.clientY - rect.top;
       
-    drawCoordinates(x*1000,y*1000);
+    drawCoordinates(x,y);
 
       
     // let coords2 = "W = " + x + " Y = " + y;
